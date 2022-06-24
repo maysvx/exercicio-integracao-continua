ca1bb5f2fbc6fd7df4b5bdef07fa1d37e02bf00a
Mayara <mayara.ceet@gmail.com>
2022-06-24 16:45:13 -0300

    conclusão dos calculos

--- math.js
+++ math.js
@@ -1,14 +1,17 @@
 function soma(a, b) {
-    a + b 
+    return a + b;
 
 }
 function subtracao(a, b) {
+    return a - b;
 }
 
 function divisao(a, b) {
+    return a / b; 
 }
 
 function multiplicacao(a, b) {
+    return a * b ;
 }
 
 module.exports = {
